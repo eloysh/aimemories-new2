3016d2ee931dea9cc48262f0428a73bc73de2f5b
Евгений <eloysh8814@gmail.com>
2025-11-02 23:54:50 +1000

    fix: move robots/sitemap to route files, remove duplicate default exports

--- app/robots.js
+++
@@ -1,7 +0,0 @@
-// app/robots.js
-export default function robots() {
-  return {
-    rules: [{ userAgent: "*", allow: "/" }],
-    sitemap: (process.env.NEXT_PUBLIC_SITE_URL || "https://www.aimemories.ru") + "/sitemap.xml",
-  };
-}
