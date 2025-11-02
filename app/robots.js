ea266a97e92ba81177a0a3668f5dec690216f235
Евгений <eloysh8814@gmail.com>
2025-11-03 00:02:37 +1000

    fix: move robots and sitemap to route files (robots.txt, sitemap.xml)

--- app/robots.js
+++
@@ -1,9 +0,0 @@
-export function GET() {
-  const body = `User-agent: *
-Allow: /
-Sitemap: https://www.aimemories.ru/sitemap.xml
-`;
-  return new Response(body, {
-    headers: { "Content-Type": "text/plain; charset=utf-8" },
-  });
-}
